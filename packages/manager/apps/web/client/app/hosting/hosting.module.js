67a24a0ca111cac4a883856c08ae13061a4f80ff
Axel Peter <axel.peter@live.com>
2019-11-05 17:22:37 +0100

    feat(hosting.sql-database.order): prepare sql database order in hosting
    Signed-off-by: Axel Peter <axel.peter@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/hosting.module.js
+++ packages/manager/apps/web/client/app/hosting/hosting.module.js
@@ -2,6 +2,7 @@ import routing from './hosting.routes';
 
 import cdn from './cdn/hosting-cdn.modules';
 import generalInformations from './general-informations/general-informations.module';
+import sqlDatabase from './sql-database/hosting-sql-database.module';
 import websiteCoach from './website-coach/website-coach.module';
 
 const moduleName = 'ovhManagerHosting';
@@ -10,6 +11,7 @@ angular
   .module(moduleName, [
     cdn,
     generalInformations,
+    sqlDatabase,
     websiteCoach,
   ])
   .config(routing);
