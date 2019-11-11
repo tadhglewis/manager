3ffda66dee58ed7699c48e44ff8b87ed777a55ee
Ravindra.Adireddy <raadired@LAPTOPC02SY20FGTFL.local>
2019-11-11 11:48:42 +0530

    fix(cloud): content-language header
    do not set content-language header for CORS requests

--- packages/manager/modules/core/src/translate/translate.factory.js
+++ packages/manager/modules/core/src/translate/translate.factory.js
@@ -1,13 +1,22 @@
+import get from 'lodash/get';
+import startsWith from 'lodash/startsWith';
+
 import {
   HEADER_LOCALE,
 } from './translate.constants';
 
 export default /* @ngInject */ TranslateService => ({
-  request: config => ({
-    ...config,
-    headers: {
-      ...config.headers,
-      [HEADER_LOCALE]: TranslateService.getUserLocale(),
-    },
-  }),
+  request: (config) => {
+    const headerConfig = {
+      ...config,
+      headers: {
+        ...config.headers,
+      },
+    };
+    // do not set content-language header for CORS requests
+    if (!startsWith(get(headerConfig, 'url'), 'https://')) {
+      headerConfig.headers[HEADER_LOCALE] = TranslateService.getUserLocale();
+    }
+    return headerConfig;
+  },
 });
